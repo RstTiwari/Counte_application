977cd1933d7a468faa7674fb8e92f0f73a2e4ec2
Rst Tiwari <79466131+RstTiwari@users.noreply.github.com>
2021-11-01 10:43:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+count = 0;
+let countEl = document.getElementById("count-el");
+let saveEL = document.getElementById("save-el");
+
+function increment(){
+    count += 1
+    countEl.textContent = count;
+
+}
+
+function save(){
+    countStr = count + ",";
+    saveEL.textContent += countStr
+    countEl.textContent =0;
+    count = 0;
+
+}
+
