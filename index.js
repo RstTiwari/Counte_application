4c5b0953c34b82740f35b57043a8471ec3f9b7d8
Rst Tiwari <79466131+RstTiwari@users.noreply.github.com>
2021-11-06 22:07:45 +0530

    new feature
    added new feature  of decrement button

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ count = 0;
 let countEl = document.getElementById("count-el");
 let saveEL = document.getElementById("save-el");
 
+
 function increment(){
     count += 1
     countEl.textContent = count;
@@ -14,5 +15,9 @@ function save(){
     countEl.textContent =0;
     count = 0;
 
+}
+function decrement(){
+    count -= 1
+    countEl.textContent = count
 }
 
